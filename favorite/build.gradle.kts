9757613551138608057a778b8fdb38c60dced0b1
divaamwall <m.sdiva524@gmail.com>
2026-02-20 21:18:50 +0700

    Add: Adding Lottie for empty data favorite and Adding Shimmer for loading item list

--- favorite/build.gradle.kts
+++ favorite/build.gradle.kts
@@ -55,6 +55,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
 
+    implementation(libs.lottie)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
